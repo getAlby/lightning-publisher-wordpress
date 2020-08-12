426c1ebee854f0eb420046956b1eeedc82360850
Yannick <musayann@octangrp.com>
2020-08-12 20:11:46 +0200

    Fix linting issues

--- js/publisher.js
+++ js/publisher.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener("DOMContentLoaded", function () {
   var checkPaidInterval = null;
   var wp_ajax_url = LN_Paywall.ajax_url;
 
@@ -32,50 +32,64 @@ window.addEventListener('DOMContentLoaded', function () {
   function checkPaymentStatus(invoice) {
     return function () {
       fetch(wp_ajax_url, {
-        method: 'POST',
-        credentials: 'same-origin',
-        cache: 'no-cache',
-        body: 'action=lnp_check_payment&token=' + invoice.token,
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
-      })
-       .then(function (response) {
-         if (response.ok) {
-            response.json().then(function (content) {
-              showContent(invoice.post_id, content);
-            });
-         }
-       });
+        method: "POST",
+        credentials: "same-origin",
+        cache: "no-cache",
+        body: "action=lnp_check_payment&token=" + invoice.token,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        },
+      }).then(function (response) {
+        if (response.ok) {
+          response.json().then(function (content) {
+            showContent(invoice.post_id, content);
+          });
+        }
+      });
     };
   }
 
   function showContent(postId, content) {
     stopWatchingForPayment();
-    var wrapper = document.querySelector('.wp-lnp-wrapper[data-lnp-postid="' + postId + '"]');
+    var wrapper = document.querySelector(
+      '.wp-lnp-wrapper[data-lnp-postid="' + postId + '"]'
+    );
     if (wrapper) {
       wrapper.outerHTML = content;
     }
   }
 
   function showQRCode(invoice) {
-    var button = document.querySelector('.wp-lnp-wrapper[data-lnp-postid="' + invoice.post_id + '"] button.wp-lnp-btn');
-    button.outerHTML = '<div class="wp-lnp-qrcode"><img src="https://chart.googleapis.com/chart?&chld=M|0&cht=qr&chs=200x200&chl=' +invoice.payment_request + '"></div>';
+    var button = document.querySelector(
+      '.wp-lnp-wrapper[data-lnp-postid="' +
+        invoice.post_id +
+        '"] button.wp-lnp-btn'
+    );
+    button.outerHTML =
+      '<div class="wp-lnp-qrcode"><img src="https://chart.googleapis.com/chart?&chld=M|0&cht=qr&chs=200x200&chl=' +
+      invoice.payment_request +
+      '"></div>';
   }
 
   function requestInvoice(postId) {
     fetch(wp_ajax_url, {
-      method: 'POST',
-      credentials: 'same-origin',
-      cache: 'no-cache',
-      body: 'action=lnp_invoice&post_id=' + postId,
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
+      method: "POST",
+      credentials: "same-origin",
+      cache: "no-cache",
+      body: "action=lnp_invoice&post_id=" + postId,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      },
     })
-    .then(function (resp) { return resp.json(); })
-    .then(function (invoice) {
-      pay(invoice);
-    })
-    .catch(function (err) {
-      throw err;
-    });
+      .then(function (resp) {
+        return resp.json();
+      })
+      .then(function (invoice) {
+        pay(invoice);
+      })
+      .catch(function (err) {
+        throw err;
+      });
   }
 
   document
@@ -94,9 +108,11 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 
   if (localStorage.getItem("wplnp_autopay")) {
-    var wrappers = document.querySelectorAll('.wp-lnp-wrapper[data-lnp-postid]');
+    var wrappers = document.querySelectorAll(
+      ".wp-lnp-wrapper[data-lnp-postid]"
+    );
     if (wrappers.length === 1) {
-      var wrapper = wrappers[0]
+      var wrapper = wrappers[0];
       var button = wrapper.querySelector("button.wp-lnp-btn");
       button.setAttribute("disabled", "");
       requestInvoice(wrapper.dataset.lnpPostid);
