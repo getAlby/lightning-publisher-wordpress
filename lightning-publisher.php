14c3d6fed43f3fd98b652bcc50db846bbbb6cdfa
musayann <musayann@octangrp.com>
2021-10-12 17:59:36 +0200

    fix: undefined wplnp value

--- lightning-publisher.php
+++ lightning-publisher.php
@@ -168,7 +168,12 @@ class WP_LN_Paywall {
   }
 
   public static function has_paid_for_all() {
-    $wplnp = isset($_COOKIE['wplnp']) ? $_COOKIE['wplnp'] : $_GET['wplnp'];
+    $wplnp = null;
+    if (isset($_COOKIE['wplnp'])) {
+      $wplnp = $_COOKIE['wplnp'];
+    } elseif (isset($_GET['wplnp'])) {
+      $wplnp = $_GET['wplnp'];
+    }
     if (empty($wplnp)) return false;
     try {
       $jwt = JWT::decode($wplnp, WP_LN_PAYWALL_JWT_KEY, array('HS256'));
