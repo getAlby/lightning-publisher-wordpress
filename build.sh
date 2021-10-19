74d13a3d9487a90ac64b64a80594ce5199047a79
Michael Bumann <hello@michaelbumann.com>
2021-10-19 23:15:59 +0200

    Fix build script to ignore .git directory

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 composer install --no-dev
 #zip -r wordpress-lightning-publisher.zip composer.json lightning-publisher.php vendor css js README.md --exclude='**/.git/*'
-zip -r wordpress-lightning-publisher.zip . --exclude='**/.git/*'
+zip -r wordpress-lightning-publisher.zip . --exclude='.git/*'
 
 
