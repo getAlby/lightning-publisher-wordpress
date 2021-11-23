aea9ef5c50a25bd2ed0f2d2620c4c8b370aea06e
musayann <musayann@octangrp.com>
2021-11-23 22:33:03 +0200

    fix: admin settings not saving

--- admin/connections.php
+++ admin/connections.php
@@ -2,19 +2,17 @@
 
 require_once 'SettingsPage.php';
 
-class ConnectionPage implements SettingsPage
+class ConnectionPage extends SettingsPage
 {
-    private $settings_path = 'lnp_settings_connection';
+    protected $settings_path = 'lnp_settings';
+    protected $option_name = 'lnp_connection';
+    
+    protected $page_title = 'Lighting Paywall Settings';
+    protected $menu_title = 'Connection';
 
-    public function __construct($plugin)
+    public function init_fields()
     {
-        $this->plugin = $plugin;
-        $this->options = get_option('lnp');
-    }
-
-    public function initFields()
-    {
-        register_setting('lnp', $this->settings_path);
+        parent::init_fields();
         add_settings_section('lnd', 'LND Config', null, $this->settings_path);
         add_settings_field('lnd_address', 'Address', array($this, 'field_lnd_address'), $this->settings_path, 'lnd');
         add_settings_field('lnd_macaroon', 'Macaroon', array($this, 'field_lnd_macaroon'), $this->settings_path, 'lnd');
@@ -33,13 +31,9 @@ class ConnectionPage implements SettingsPage
         add_settings_field('lndhub_password', 'Lndhub Password', array($this, 'field_lndhub_password'), $this->settings_path, 'lndhub');
     }
 
-    public function initPage()
-    {
-        add_submenu_page('lnp_settings', 'Lighting Paywall Settings', 'Connection', 'manage_options', 'lnp_settings', array($this, 'renderer'));
-    }
-
     public function renderer()
     {
+        
 ?>
         <div class="wrap">
             <h1>Lightning Connection Settings</h1>
@@ -87,64 +81,72 @@ class ConnectionPage implements SettingsPage
     {
         $help = 'e.g. https://127.0.0.1:8080 - or <a href="#" id="load_from_lndconnect">click here to load details from a lndconnect</a>';
         printf(
-            '<input type="text" name="lnp[lnd_address]" id="lnp_lnd_address" value="%s" autocomplete="off" /><br>%s',
-            esc_attr($this->options['lnd_address']),
+            '<input type="text" name="%s" id="lnp_lnd_address" value="%s" autocomplete="off" /><br>%s',
+            $this->get_field_name('lnd_address'),
+            $this->get_field_value('lnd_address'),
             $help
         );
     }
     public function field_lnd_macaroon()
     {
         printf(
-            '<input type="text" name="lnp[lnd_macaroon]" id="lnp_lnd_macaroon" value="%s" autocomplete="off" /><br><label>%s</label>',
-            esc_attr($this->options['lnd_macaroon']),
+            '<input type="text" name="%s" id="lnp_lnd_macaroon" value="%s" autocomplete="off" /><br><label>%s</label>',
+            $this->get_field_name('lnd_macaroon'),
+            $this->get_field_value('lnd_macaroon'),
             'Invoices macaroon in HEX format'
         );
     }
     public function field_lnd_cert()
     {
         printf(
-            '<input type="text" name="lnp[lnd_cert]" value="%s" id="lnp_lnd_cert" autocomplete="off" /><br><label>%s</label>',
-            esc_attr($this->options['lnd_cert']),
+            '<input type="text" name="%s" value="%s" id="lnp_lnd_cert" autocomplete="off" /><br><label>%s</label>',
+            $this->get_field_name('lnd_macaroon'),
+            $this->get_field_value('lnd_cert'),
             'TLS Certificate'
         );
     }
     public function field_lnbits_apikey()
     {
         printf(
-            '<input type="text" name="lnp[lnbits_apikey]" value="%s" autocomplete="off" /><br><label>%s</label>',
-            esc_attr($this->options['lnbits_apikey']),
+            '<input type="text" name="%s" value="%s" autocomplete="off" /><br><label>%s</label>',
+            $this->get_field_name('lnbits_apikey'),
+            $this->get_field_value('lnbits_apikey'),
             'LNbits Invoice/read key'
         );
     }
     public function field_lnaddress_address()
     {
         printf(
-            '<input type="text" name="lnp[lnaddress_address]" value="%s" autocomplete="off" /><br><label>%s</label>',
-            esc_attr($this->options['lnaddress_address']),
+            '<input type="text" name="%s" value="%s" autocomplete="off" /><br><label>%s</label>',
+            $this->get_field_name('lnaddress_address'),
+            $this->get_field_value('lnaddress_address'),
             'Lightning Address (e.g. you@payaddress.co) - only works if the vistor supports WebLN!'
         );
     }
     public function field_lndhub_url()
     {
         printf(
-            '<input type="text" name="lnp[lndhub_url]" value="%s" autocomplete="off" /><br><label>%s</label>',
-            esc_attr($this->options['lndhub_url']),
+            '<input type="text" name="%s" value="%s" autocomplete="off" /><br><label>%s</label>',
+            $this->get_field_name('lndhub_url'),
+            $this->get_field_value('lndhub_url'),
             'Lndhub Url'
         );
     }
     public function field_lndhub_login()
     {
         printf(
-            '<input type="text" name="lnp[lndhub_login]" value="%s" autocomplete="off" /><br><label>%s</label>',
-            esc_attr($this->options['lndhub_login']),
+            '<input type="text" name="%s" value="%s" autocomplete="off" /><br><label>%s</label>',
+            $this->get_field_name('lndhub_login'),
+            $this->get_field_value('lndhub_login'),
             'Lndhub Login'
         );
     }
     public function field_lndhub_password()
     {
         printf(
-            '<input type="password" name="lnp[lndhub_password]" value="%s" autocomplete="off" /><br><label>%s</label>',
-            esc_attr($this->options['lndhub_password']),
+            '<input type="password" name="%s" value="%s" autocomplete="off" /><br><label>%s</label>',
+            $this->get_field_name('lndhub_password'),
+            $this->get_field_value('lndhub_password'),
             'Lndhub Password'
         );
     }
