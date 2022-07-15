83b44d9d259a2f1311886518168c4b97550128ab
Moritz Kaminski <moritz.kaminski@gmail.com>
2022-07-15 17:15:38 +0200

    Update page-help.php
    added more information to the help page

--- admin/templates/settings/page-help.php
+++ admin/templates/settings/page-help.php
@@ -24,4 +24,19 @@ defined('WPINC') || die; ?>
         <p>[lnpaywall] eg: [lnpaywall amount="100"]</p>
     </blockquote>
 
+    <h3>Wallet Settings</h3>
+    <p>
+      You have the option to receive your payments in the following wallets:
+    </p>
+    <blockquote>
+        <ul>
+            <li>Alby Wallet: Alby provides an easy to use all-in one service and manages a Lightning wallet for you. More information: https://getalby.com/</li>
+            <li>LND: The Lightning Network Daemon (LND) is one implementation of several of a Lightning Network node. If you want to connect a LND node make sure to have enough incoming liquidity to receive all payments.</li>
+            <li>LNDHub: A free and open source, multiple account plugin for Lightning built on top of the Lightning Network Deamon. It allows that a group of users run different accounts with only one node in a trust-minimized setup. More information: https://github.com/getAlby/lndhub.go</li>
+            <li>LNbits: A free and open-source Lightning accounts system with extensions. More information: https://lnbits.com/</li>
+            <li>BTCPay: A self-hosted, open-source cryptocurrency payment processor. More information: https://btcpayserver.org/</li>
+            <li>Lightning Address: A simple way for anyone to send you bitcoin instantly on the Lightning Network.It looks like a normal email address such as you@payaddress.com. But it only works if the website visitor uses a WebLN enabled wallet to pay. More information: https://lightningaddress.com/</li>
+        </ul>
+    </blockquote>
+
 </div>
