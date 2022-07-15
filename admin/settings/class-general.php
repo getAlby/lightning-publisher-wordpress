7653d4fdeb78db093291d57a2047e74b340f4d2c
Moritz Kaminski <moritz.kaminski@gmail.com>
2022-07-15 20:14:10 +0200

    Update class-general.php

--- admin/settings/class-general.php
+++ admin/settings/class-general.php
@@ -52,8 +52,8 @@ class LNP_GeneralPage extends LNP_SettingsPage
                 'type'  => 'checkbox',
                 'name'  => 'lnurl_meta_tag',
                 'value' => 'on',
-                'label' => __('Enable Value4Value lightning meta tag', 'lnp-alby'),
-                'description' => __('Enable the lightning metatag which allows visitor to send sats to your page', 'lnp-alby'),
+                'label' => __('Enable Value 4 Value Lightning meta tag', 'lnp-alby'),
+                'description' => __('Enable the Lightning metatag which allows visitor to send sats to your page.', 'lnp-alby'),
             ),
         );
 
@@ -61,8 +61,8 @@ class LNP_GeneralPage extends LNP_SettingsPage
             'tab'     => 'value4value',
             'field'   => array(
                 'name'  => 'lnurl_meta_tag_lnurlp',
-                'label' => __('Custom recipient for the lightning meta tag', 'lnp-alby'),
-                'description' => __('By default the connected wallet is used to generate the meta tag. You can overwrite this here for example with your lightning address', 'lnp-alby'),
+                'label' => __('Custom recipient for the Lightning meta tag', 'lnp-alby'),
+                'description' => __('By default the connected wallet is used to generate the meta tag. You can overwrite this here for example with your Lightning Address.', 'lnp-alby'),
             ),
         );
 
@@ -72,8 +72,8 @@ class LNP_GeneralPage extends LNP_SettingsPage
                 'type'  => 'checkbox',
                 'name'  => 'add_v4v_rss_tag',
                 'value' => 'on',
-                'label' => __('Enable Value4Value tag', 'lnp-alby'),
-                'description' => __('Add the podcast:value tag to your RSS feed. Configure the node key (and custom key/value if needed)', 'lnp-alby'),
+                'label' => __('Enable Value 4 Value tag', 'lnp-alby'),
+                'description' => __('Add the podcast:value tag to your RSS feed. Configure the node address (and custom key/value if needed).', 'lnp-alby'),
             ),
         );
 
@@ -81,8 +81,8 @@ class LNP_GeneralPage extends LNP_SettingsPage
             'tab'     => 'value4value',
             'field'   => array(
                 'name'  => 'v4v_node_key',
-                'label' => __('Node key', 'lnp-alby'),
-                'description' => __('Node address - the lightning node public key', 'lnp-alby'),
+                'label' => __('Node Address', 'lnp-alby'),
+                'description' => __('Node address - the Lightning node public key', 'lnp-alby'),
             ),
         );
 
@@ -106,7 +106,7 @@ class LNP_GeneralPage extends LNP_SettingsPage
             'field'   => array(
                 'name'  => 'cookie_timeframe_days',
                 'label' => __('Cookie timeframe', 'lnp-alby'),
-                'description' => __('Paid articles are saved in a cookie. How many days should these cookie be valid? (default: 180)', 'lnp-alby'),
+                'description' => __('Paid articles are saved in a cookie. How many days should these cookies be valid? (default: 180)', 'lnp-alby'),
             ),
         );
 
