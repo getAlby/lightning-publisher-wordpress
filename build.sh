c12eeaa2e5944b9ea184ce99184adcdae38a5402
Michael Bumann <hello@michaelbumann.com>
2022-08-11 16:31:43 +0200

    Prepare build script for svn release

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 composer install --no-dev
-#zip -r wordpress-lightning-publisher.zip composer.json lightning-publisher.php vendor css js README.md --exclude='**/.git/*'
-zip -r bitcoin-lightning-publisher.zip . --exclude='.git/*'
+echo "Please provide the path the dist folder"
+read dist
+
+echo "Copying plugin to $dist"
+rsync -av --exclude='.git/*' --exclude='./dist' . $dist
+# zip -r wordpress-lightning-publisher.zip composer.json lightning-publisher.php vendor css js README.md --exclude='**/.git/*'
+echo "Zipping plugin from $dist"
+zip -r bitcoin-lightning-publisher.zip $dist --exclude='.git/*'
+echo "Done"
 
 
