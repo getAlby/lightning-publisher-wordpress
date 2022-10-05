33ebd3b1f959382a03a4cd5cf48b975688964139
Michael Bumann <hello@michaelbumann.com>
2022-10-05 19:20:56 +0200

    Update dist script

--- prepare-dist.sh
+++ prepare-dist.sh
@@ -4,7 +4,7 @@ echo "Please provide the path the dist folder"
 read dist
 
 echo "Copying plugin to $dist"
-rsync -av --exclude='.git/*' --exclude='./dist' --exclude='.gitignore' --exclude='.git' . $dist
+rsync -av --exclude='.git/*' --exclude='./dist' --exclude='.gitignore' --exclude='.git' --exclude='bitcoin-lightning-publisher.zip' . $dist
 echo "Done"
 
 
